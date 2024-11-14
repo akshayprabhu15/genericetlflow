bb8b00f23e54e54556364481bacad651c13f8461
yashmhatre01@gmail.com <yashmhatre01@gmail.com>
2024-11-14 14:46:38 +0000

    cleaned notebooks

--- DataCheck.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-from Pytest import DataValidator
-
-config_file_path = dbutils.widgets.get("config_file_path")
-
-datacheck = DataValidator(spark)
-datacheck.validate_rawdata(config_file_path)
-datacheck.validate_duplicate(config_file_path)
